e95d2fd7b3e1e0cc9fa381f1b8bfba86c87a6ade
sloway <touchthesky80s@gmail.com>
2022-11-10 00:53:59 +0900

    ch4.0~4.2

--- schema.js
+++ schema.js
@@ -4,11 +4,10 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries, mutations}.js`
+  `${__dirname}/**/*.{queries,mutations}.js`
 );
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
-
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 export default schema;
---
+++ users/users.mutations.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcrypt";
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, userName, email, password }
+    ) => {
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [{ userName }, { email }],
+        },
+      });
+      const uglyPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: { userName, email, firstName, lastName, password: uglyPassword },
+      });
+    },
+  },
+};
